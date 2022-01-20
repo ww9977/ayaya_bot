7b8c2804e4660356dcd7b23487dbda10f163f224
ww9977 <linwillie9977@gmail.com>
2022-01-21 02:45:03 +0800

    修改前置

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
-async def on_ready()
-    print(">> [Bot online] <<")
+async def on_ready():
+    print("[Bot online]")
 
 bot.run('OTMzMzQ3NDcxNTMzNDI4Nzg2.YegNkQ.QZgWVq5dHzUPr3VHDOcchA07DaI')
