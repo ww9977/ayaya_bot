a288570ad7fad28dd0babe952386f0efaef04a14
ww9977 <linwillie9977@gmail.com>
2022-01-21 03:51:18 +0800

    我也不知道啥 反正就指定時間的前置ㄅ

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import discord
 from discord.ext import commands
+from core.classes import Cog_Extension
+import json,datetime,asyncio
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents = intents)
 
 @bot.event
 async def on_ready():
     print("[Bot online]")
+
+@bot.event
+async def 
+
 
 bot.run('OTMzMzQ3NDcxNTMzNDI4Nzg2.YegNkQ.QZgWVq5dHzUPr3VHDOcchA07DaI')
