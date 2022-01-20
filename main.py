9c6d589bb4f7f96128188fd548aa96c477ae3420
ww9977 <linwillie9977@gmail.com>
2022-01-21 02:33:19 +0800

    機器人 前置

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-hello
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready()
+    print(">> [Bot online] <<")
+
+bot.run('OTMzMzQ3NDcxNTMzNDI4Nzg2.YegNkQ.QZgWVq5dHzUPr3VHDOcchA07DaI')
