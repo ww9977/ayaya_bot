83a2f452ff316f96ba942a5395df8da61ae2b21c
ww9977 <linwillie9977@gmail.com>
2022-01-22 05:04:53 +0800

    新增計時器(物理

--- cmds/time_text.py
+++ cmds/time_text.py
@@ -1,20 +1,26 @@
+from time import sleep
 import discord
 from discord.ext import commands
 from core.classes import Cog_Ext
-import json,asyncio,datetime
+from datetime import datetime
+import json,asyncio
+
 
 class time_text(Cog_Ext):
     def __init__(self, *args,**kwargs):
         super().__init__(*args,**kwargs)
 
         async def FFXIV_remind():
-            await self.bot.wait_until_ready()
-            self.channel=self.bot.get_channel(933926989298008134)
-            while not self.bot.is_closed():
-                await self.channel.send("running")
-                await asyncio.sleep(3)
+         await self.bot.wait_until_ready()
+         while not self.bot.is_closed():
+             if datetime.today().isoweekday() == 6:
+                 self.channel=self.bot.get_channel(933926989298008134)
+                 await self.channel.send('abyss')
+             sleep(59)    
+         
     
         self.bg_task=self.bot.loop.create_task(FFXIV_remind())
+    
 
 def setup(bot):
     bot.add_cog(time_text(bot))
